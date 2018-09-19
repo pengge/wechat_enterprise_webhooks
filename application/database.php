242f23be54b037e2b3eb96addd17e968d3ef6c5d
Sam <530566495@qq.com>
2018-09-19 13:44:40 +0800

    去掉数据

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type' => 'mysql',
     // 服务器地址
-    'hostname' => '172.21.0.14',
+    'hostname' => '127.0.0.1',
     // 数据库名
-    'database' => 'hycheck',
+    'database' => 'logdb',
     // 用户名
-    'username' => 'hycheck',
+    'username' => 'root',
     // 密码
-    'password' => 'yD7W6E9Grq9gotC8',
+    'password' => '',
     // 端口
     'hostport' => '3306',
     // 连接dsn
